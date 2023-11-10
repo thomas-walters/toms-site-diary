b843cac089da4624746d6b4a8f2c746131fac291
thomas-walters <thomasgrantwalters@gmail.com>
2023-11-10 17:42:23 +1100

    update README

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import DiaryList from './components/diary_list'
 import Link from 'next/link'
 
 export default function Home() {
-
   return (
     <div className="flex items-center justify-between flex-col">
       <div className="flex sm:flex-row flex-col justify-between gap-2 sm:w-screen sm:max-w-7xl sm:px-24 sm:mb-6">
